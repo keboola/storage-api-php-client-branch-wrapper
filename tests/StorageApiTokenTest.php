2e476ad43fb05f62be21c74173a69984b9237224
Ondrej Popelka <odinuv@gmail.com>
2023-11-09 14:12:50 +0100

    Add StorageApiToken class (again and again, when this will end?) https://keboola.atlassian.net/browse/GCP-462

---
+++ tests/StorageApiTokenTest.php
@@ -0,0 +1,197 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\StorageApiBranch\Tests;
+
+use Keboola\StorageApiBranch\StorageApiToken;
+use PHPUnit\Framework\TestCase;
+
+class StorageApiTokenTest extends TestCase
+{
+    public function testAccessors(): void
+    {
+        $token = new StorageApiToken(
+            [
+                'id' => '123',
+                'description' => 'token description',
+                'canManageBuckets' => true,
+                'canManageTokens' => false,
+                'canCreateJobs' => true,
+                'owner' => [
+                    'id' => '456',
+                    'name' => 'my project',
+                    'features' => ['foo', 'bar'],
+                    'payAsYouGo' => [
+                        'purchasedCredits' => 1.23,
+                    ],
+                    'fileStorageProvider' => 'aws',
+                ],
+                'admin' => [
+                    'samlParameters' => [
+                        'userId' => '789',
+                    ],
+                    'role' => 'admin',
+                ],
+                'componentAccess' => ['keboola.component'],
+            ],
+            'tokenValue',
+        );
+
+        self::assertSame('123', $token->getTokenId());
+        self::assertSame('tokenValue', $token->getTokenValue());
+        self::assertSame('456', $token->getProjectId());
+        self::assertSame(['foo', 'bar'], $token->getFeatures());
+        self::assertTrue($token->hasFeature('foo'));
+        self::assertFalse($token->hasFeature('baz'));
+        self::assertSame(1.23, $token->getPayAsYouGoPurchasedCredits());
+        self::assertSame('789', $token->getSamlUserId());
+        self::assertSame('aws', $token->getFileStorageProvider());
+        self::assertSame('my project', $token->getProjectName());
+        self::assertSame('token description', $token->getTokenDesc());
+        self::assertSame('admin', $token->getRole());
+        self::assertSame(['admin'], $token->getRoles());
+        self::assertSame(['keboola.component'], $token->getAllowedComponents());
+        self::assertSame(['canManageBuckets', 'canCreateJobs'], $token->getPermissions());
+
+        self::assertSame(
+            [
+                'id' => '123',
+                'description' => 'token description',
+                'canManageBuckets' => true,
+                'canManageTokens' => false,
+                'canCreateJobs' => true,
+                'owner' => [
+                    'id' => '456',
+                    'name' => 'my project',
+                    'features' => ['foo', 'bar'],
+                    'payAsYouGo' => [
+                        'purchasedCredits' => 1.23,
+                    ],
+                    'fileStorageProvider' => 'aws',
+                ],
+                'admin' => [
+                    'samlParameters' => [
+                        'userId' => '789',
+                    ],
+                    'role' => 'admin',
+                ],
+                'componentAccess' => ['keboola.component'],
+            ],
+            $token->getTokenInfo(),
+        );
+    }
+
+    public function testAccessorsTypeZealous(): void
+    {
+        $token = new StorageApiToken(
+            [
+                'id' => 123,
+                'description' => 'token description',
+                'canCreateJobs' => true,
+                'canManageBuckets' => true,
+                'youcanttouchthis' => true,
+                'canManageBuckets"' => true,
+                'owner' => [
+                    'id' => 456,
+                    'name' => 'my project',
+                    'payAsYouGo' => [
+                        'purchasedCredits' => 1,
+                    ],
+                ],
+                'admin' => [
+                    'samlParameters' => [
+                        'userId' => 789,
+                    ],
+                ],
+            ],
+            'tokenValue',
+        );
+
+        self::assertSame(['canCreateJobs', 'canManageBuckets'], $token->getPermissions());
+        self::assertSame('123', $token->getTokenId());
+        self::assertSame('456', $token->getProjectId());
+        self::assertSame(1.0, $token->getPayAsYouGoPurchasedCredits());
+        self::assertSame('789', $token->getSamlUserId());
+    }
+
+    public function testPurchasedCreditsMissing(): void
+    {
+        $token = new StorageApiToken(
+            [
+                'id' => 123,
+                'description' => 'token description',
+                'owner' => [
+                    'id' => 456,
+                    'name' => 'my project',
+                ],
+            ],
+            'tokenValue',
+        );
+
+        self::assertSame(0.0, $token->getPayAsYouGoPurchasedCredits());
+    }
+
+    public function testNoRoles(): void
+    {
+        $token = new StorageApiToken(
+            [
+                'id' => '123',
+                'description' => 'token description',
+                'owner' => [
+                    'id' => '456',
+                    'name' => 'my project',
+                    'features' => ['foo', 'bar'],
+                    'payAsYouGo' => [
+                        'purchasedCredits' => 1.23,
+                    ],
+                    'fileStorageProvider' => 'aws',
+                ],
+                'admin' => [
+                    'samlParameters' => [
+                        'userId' => '789',
+                    ],
+                ],
+            ],
+            'tokenValue',
+        );
+
+        self::assertNull($token->getRole());
+        self::assertSame([], $token->getRoles());
+    }
+
+    public function testNoComponentAccessLimits(): void
+    {
+        $token = new StorageApiToken(
+            [
+                'id' => '123',
+                'owner' => [
+                    'id' => '456',
+                    'name' => 'my project',
+                    'features' => [],
+                ],
+                'componentAccess' => [],
+            ],
+            'tokenValue',
+        );
+
+        self::assertSame([], $token->getAllowedComponents());
+    }
+
+    public function testAllComponentAccessLimits(): void
+    {
+        $token = new StorageApiToken(
+            [
+                'id' => '123',
+                'owner' => [
+                    'id' => '456',
+                    'name' => 'my project',
+                    'features' => [],
+                ],
+            ],
+            'tokenValue',
+        );
+
+        self::assertSame(null, $token->getAllowedComponents());
+    }
+}
